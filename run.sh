029f466befcd422e7f86ff5395449ded8c0f91d5
Kent Daleng <kent.daleng@nav.no>
2021-06-08 06:57:16 +0200

    let every version after 10 invoke java in the hacky way

--- run.sh
+++ run.sh
@@ -2,10 +2,10 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )" # fetches the directory of this script
 
 # gets the java-binary for version 1.6 and runs the game with the set up libraries
-if [ $(sw_vers | grep "ProductVersion" | awk '{split($NF, w, "."); print w[1]}') == "11" ]; then
-    # sw_vers returns the software version, so we check if we're on 11.x.x to determine if we're on big sur
+if [ $(sw_vers | grep "ProductVersion" | awk '{split($NF, w, "."); print w[1]}') -gt "10" ]; then
+    # sw_vers returns the software version, so we check if we're on a version after 10.15 Catalina
     # and utilize a hardcoded call to java which may or may not exist
-    echo "Running on Big Sur, calling java in a hacky way (might not work)"
+    echo "Detected macOS version later than 10.15 Catalina, calling java in a hacky way (might not work)"
     /Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home/bin/java -Djava.library.path="$DIR/libs/" -jar "$DIR/cultris2.jar"
 else
     $(/usr/libexec/java_home -v 1.6)/bin/java -Djava.library.path="$DIR/libs/" -jar "$DIR/cultris2.jar"
